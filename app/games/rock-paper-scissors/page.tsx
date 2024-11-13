65cf593043e8ae86b8efab17671db7137c911e71
Hafemi <118757157+hafemi@users.noreply.github.com>
2024-11-13 13:21:53 +0100

    added page for rock paper scissors

---
+++ app/games/rock-paper-scissors/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div>
+      <title>ByteBay - Rock Paper Scissors</title>
+      <main>
+        <h1>ROCK PAPER SCISSORS</h1>
+      </main>
+    </div>
+  );
+}
