c1c82f9b2393be49f5a7732c18d36ee70c0fe808
Hafemi <118757157+hafemi@users.noreply.github.com>
2024-11-13 15:25:49 +0100

    added tools/games section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,28 @@ export default function Home() {
       <title>ByteBay</title>
       <main className={styles.main}>
         <h1 className={styles.heading}>BYTE BAY</h1>
-        <Link href="/tools/maze-generator">Maze Generator</Link>
-        <br />
-        <Link href="/games/rock-paper-scissors">Rock Paper Scissors</Link>
+
+        <section>
+          <h2>Tools</h2>
+          <ul>
+            <li>
+              <Link href="/tools/maze-generator">Maze Generator</Link>
+            </li>
+            <li>Random Number Generator (planned)</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2>Games</h2>
+          <ul>
+            <li>
+              <Link href="/games/rock-paper-scissors">Rock Paper Scissors</Link>
+            </li>
+            <li>Hangman (planned)</li>
+            <li>Tic Tac Toe (planned)</li>
+          </ul>
+        </section>
+        
       </main>
     </div>
   );
