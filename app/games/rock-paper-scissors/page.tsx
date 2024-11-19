0be0ace992acbea15f540f453add468f5d602243
Hafemi <118757157+hafemi@users.noreply.github.com>
2024-11-19 17:57:32 +0100

    RockPaperScissor - added footer

--- app/games/rock-paper-scissors/page.tsx
+++ app/games/rock-paper-scissors/page.tsx
@@ -1,12 +1,15 @@
 'use client';
+import { Footer } from '@/app/layout';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.page}>
       <title>ByteBay - Rock Paper Scissors</title>
-      <main>
+      <main className={styles.main}>
         <h1>ROCK PAPER SCISSORS</h1>
       </main>
+      <Footer />
     </div>
   );
 }
