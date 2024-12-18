4c85f35eeadcab5044968bb46bff510732450b2d
Hafemi <118757157+hafemi@users.noreply.github.com>
2024-12-18 17:53:18 +0100

    moved Header/Footer to "root-layout" component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import { Footer, Header } from '@/lib/components/root-layout';
+import { Metadata } from 'next';
 import './globals.css';
 import styles from './layout.module.css';
-import { Metadata } from 'next';
 
 export default function RootLayout({
   children,
@@ -21,55 +20,6 @@ export default function RootLayout({
     </html>
   );
 }
-
-const Header = () => {
-  return (
-    <header className={styles.header}>
-      <div
-        className={`
-        ${styles.logoContainer}
-        ${styles.headerLogoPadding}`}
-      >
-        <Image src="/assets/bytecraft_logo.png" alt="ByteCrate logo" width={70} height={70} />
-        <p>ByteCrate</p>
-      </div>
-      <nav className={styles.headerNavigation}>
-        <Link href="/">Home</Link>
-        <div className={styles.dropdown}>
-          <p>Tools</p>
-          <div className={styles.dropdownContent}>
-            <Link href="/tools/maze-generator">Maze Generator</Link>
-            <Link href="/tools/password-generator">Password Generator</Link>
-          </div>
-        </div>
-        <div className={styles.dropdown}>
-          <p>Games</p>
-          <div className={styles.dropdownContent}>
-            <Link href="/games/rock-paper-scissors">Rock Paper Scissors</Link>
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
-};
-
-const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.logoContainer}>
-        <Image src="/assets/bytecraft_logo.png" alt="ByteCrate logo" width={50} height={50} />
-        <p>ByteCrate</p>
-      </div>
-      <nav className={styles.footerNavigation}>
-        <Link href="">About us</Link>
-        <Link href="">Contact</Link>
-        <Link href="https://github.com/hafemi/byte-bay" target="_blank" rel="noopener noreferrer">
-          Github Repository
-        </Link>
-      </nav>
-    </footer>
-  );
-};
 
 export const metadata: Metadata = {
   title: 'ByteCrate',
---
+++ lib/components/root-layout.tsx
@@ -0,0 +1,71 @@
+import styles from '@/app/layout.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <HeaderLogo />
+      <HeaderNavigation />
+    </header>
+  );
+};
+
+const HeaderLogo = () => {
+  return (
+    <div className={`${styles.logoContainer} ${styles.headerLogoPadding}`}>
+      <Image src="/assets/bytecraft_logo.png" alt="ByteCrate logo" width={70} height={70} />
+      <p>ByteCrate</p>
+    </div>
+  );
+};
+
+const HeaderNavigation = () => {
+  return (
+    <nav className={styles.headerNavigation}>
+      <Link href="/">Home</Link>
+
+      <div className={styles.dropdown}>
+        <p>Tools</p>
+        <div className={styles.dropdownContent}>
+          <Link href="/tools/maze-generator">Maze Generator</Link>
+          <Link href="/tools/password-generator">Password Generator</Link>
+        </div>
+      </div>
+
+      <div className={styles.dropdown}>
+        <p>Games</p>
+      </div>
+    </nav>
+  );
+};
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <FooterLogo />
+      <FooterNavigation />
+    </footer>
+  );
+};
+
+const FooterLogo = () => {
+  return (
+    <div className={styles.logoContainer}>
+      <Image src="/assets/bytecraft_logo.png" alt="ByteCrate logo" width={50} height={50} />
+      <p>ByteCrate</p>
+    </div>
+  );
+};
+
+const FooterNavigation = () => {
+  return (
+    <nav className={styles.footerNavigation}>
+      <Link href="">About us</Link>
+      <Link href="">Contact</Link>
+      <Link href="https://github.com/hafemi/byte-bay" target="_blank" rel="noopener noreferrer">
+        Github Repository
+      </Link>
+    </nav>
+  );
+};
