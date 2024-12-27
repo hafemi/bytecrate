896c78cf3530b2ef0c1b5d0bdb0baab33fc46759
Hafemi <118757157+hafemi@users.noreply.github.com>
2024-12-27 15:17:35 +0100

    RPS - added page

---
+++ app/games/rock-paper-scissors/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div>
+      <title>ByteCrate - Rock Paper Scissors</title>
+      <main>
+        <h1>ROCK PAPER SCISSORS</h1>
+        <p>AAAAAAAAAAAAAAAAAH</p>
+      </main>
+    </div>
+  )
+}
